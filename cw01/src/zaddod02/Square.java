18c79b19ef8fb822fcd332bd311c399ae9ff8dae
r2d2-pjatk <s34918@pjwstk.edu.pl>
2026-03-14 10:31:05 +0100

    utworzono klasę Square implementującą interfejs Comparable<Square> (metoda compareTo)

---
+++ cw01/src/zaddod02/Square.java
@@ -0,0 +1,29 @@
+package zaddod02;
+
+public class Square implements Comparable<Square> {
+
+    private static int counter = 0;
+    
+    private int length;
+    private int number;
+    
+    public Square(int length) {
+        this.length = length;
+        this.number = ++counter;
+    }
+    
+    public int getArea() {
+        return Math.powExact(length, 2);
+    }
+    
+    @Override
+    public String toString() {
+        return "Square{" + "number=" + number + ", area=" + this.getArea() + '}';
+    }
+    
+    @Override
+    public int compareTo(Square other) {
+        return Integer.compare(this.getArea(), other.getArea());
+    }
+    
+}
