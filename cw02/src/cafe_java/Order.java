ef14e989881c2bdb55ff9bd19248f35fb06c95d1
R2D2-95 <s34918@pjwstk.edu.pl>
2026-03-15 13:51:10 +0100

    w klasie Order utworzono niestatyczną klasę wewnÄętrzną Receipt, która odpowiada za generowanie tekstu na paragonie

--- cw02/src/cafe_java/Order.java
+++ cw02/src/cafe_java/Order.java
@@ -1,6 +1,7 @@
 package cafe_java;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Order {
 
@@ -87,6 +88,87 @@ public class Order {
     @Override
     public String toString() {
         return String.format("%d, %s, %d, %.2f", id, customer.name(), this.getItemCount(), this.calculateTotal());
+    }
+
+    public class Receipt {
+
+        private static final String CAFE_NAME = "KAWIARNIA \"POD JAVĄ\"";
+        private static final int WIDTH = 42;
+
+        private final String cashierName;
+
+        public Receipt(String cashierName) {
+            if (cashierName == null || cashierName.isBlank()) {
+                throw new IllegalArgumentException("imię kasjera nie może być puste");
+            } else {
+                this.cashierName = cashierName;
+            }
+        }
+
+        private String center(String text) {
+            int leadingSpacesLength = Math.max((WIDTH - text.length()) / 2, 0);
+            return " ".repeat(leadingSpacesLength) + text;
+        }
+
+        private String formatLine(String label, double amount) {
+            String leftSide = '\t' + label;
+            String rightSide = String.format("%.2f zł", amount);
+
+            int numOfSpacesToAlign = Math.max(WIDTH - (leftSide.length() + rightSide.length()), 1);
+            return leftSide + " ".repeat(numOfSpacesToAlign) + rightSide;
+        }
+
+        private String formatNegLine(String label, double amount) {
+            String leftSide = '\t' + label;
+            String rightSide = String.format("-%.2f zł", amount);
+
+            int numOfSpacesToAlign = Math.max(WIDTH - (leftSide.length() + rightSide.length()), 1);
+            return leftSide + " ".repeat(numOfSpacesToAlign) + rightSide;
+        }
+
+        private String generate() {
+            StringBuilder sb = new StringBuilder();
+            String sep1 = "=".repeat(WIDTH);
+            String sep2 = "-".repeat(WIDTH);
+
+            sb.append(sep1 + '\n' + this.center(CAFE_NAME) + '\n' + sep1 + '\n');
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+            sb.append(formatter.format(createdAt));
+
+            sb.append('\n' + cashierName + '\n');
+
+            sb.append(id + '\n');
+
+            sb.append(customer.name() + '\n');
+            sb.append(customer.name() + ' ' + customer.loyaltyLevel() + '\n');
+            sb.append("\n");
+
+            for (OrderItem item : items) {
+                sb.append(item.formatted() + '\n');
+            }
+
+            sb.append('\n');
+            sb.append(sep2 + "\n");
+
+            double subtotal = calculateSubtotal();
+            sb.append(formatLine("Suma:", subtotal) + '\n');
+
+            if (discount != null) {
+                double savings = discount.savings(subtotal);
+                sb.append(
+                        formatNegLine("Rabat: " + discount.getDescription(), savings) + '\n'
+                );
+            }
+
+            sb.append(sep2 + '\n');
+            sb.append(formatLine("DO ZAPŁATY:", calculateTotal()) + '\n');
+
+            sb.append(sep1 + '\n' + this.center("Dziękujemy!") + '\n' + sep1 + '\n');
+
+            return sb.toString();
+        }
+
     }
 
 }
